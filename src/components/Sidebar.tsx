82a5fe4053718b5c581feee007ff7ab61e5b300f
jobson <jammeloalves@gmail.com>
2023-02-10 08:35:17 -0300

    botao menu respomsivel pronto

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
-import { BsChevronRight, BsFillArrowDownCircleFill, BsFillArrowUpCircleFill, BsFillHouseFill, BsGraphUp, BsTools } from "react-icons/bs"
-import { RiLogoutCircleRLine } from "react-icons/ri";
+import { BsChevronRight, BsFillArrowDownCircleFill, BsFillArrowUpCircleFill, BsFillGridFill, BsFillHouseFill, BsPieChartFill, BsTools } from "react-icons/bs"
+import { RiCloseLine, RiLogoutCircleRLine, RiMenuLine,} from "react-icons/ri";
 
 function Sidebar() {
+  const [showMenu, setShowMenu] = useState(false)
+  const [showSubmenu, setShowSubmenu] = useState(false)
   return (
-    <div className='xl:h-[100vh] fixed xl:static w-full h-full -left-full p-5 top-0 flex flex-col justify-between bg-gray4' >
+    <>
+    <div className={`xl:h-[100vh] fixed xl:static w-[70%] md:w-[40%] lg:w[30%] xl:w-auto h-full p-5 top-0 flex flex-col justify-between z-50 bg-gray4
+    ${showMenu ? "left-0" : "left-full"} transition-all`} >
       <div>
         <h1 className='text-center text-2xl font-bol text-white mb-10'>
           ADMIN<span className='text-green-500 text-4xl'>@</span>
@@ -16,9 +20,9 @@ function Sidebar() {
               <BsFillHouseFill className='text-green-500 ' />Home
             </Link>
           </li>
-         <li>
+         <li> 
             <Link to="/dashboard" className='flex items-center gap-2 py-2 px-4 rounded-lg text-slate-200 hover:bg-primary transition-colors'>
-              <BsGraphUp className='text-green-500 ' />Dashboard
+              <BsPieChartFill className='text-green-500 ' />Dashboard
             </Link>
          </li>
          <li>
@@ -31,18 +35,24 @@ function Sidebar() {
               <BsFillArrowDownCircleFill className='text-green-500 ' />Despesas
             </Link>
          </li>
+           <li>
+            <Link to="/dashboard" className='flex items-center gap-2 py-2 px-4 rounded-lg text-slate-200 hover:bg-primary transition-colors'>
+              <BsFillGridFill className='text-green-500 ' />Categorias
+            </Link>
+         </li>
          <li>
-            <button className='flex items-center justify-between py-2 px-4 rounded-lg text-slate-200 hover:bg-primary transition-colors'>
+            <button onClick={() => setShowSubmenu(!showSubmenu)} className='flex items-center justify-between py-2 px-4 rounded-lg 
+            text-slate-200 hover:bg-primary transition-colors'>
               <span className=' flex items-center gap-2'>
                 <BsTools className='text-green-500 ' />Comfiguração
               </span>
-              <BsChevronRight />
+              <BsChevronRight className={`${showSubmenu && "rotate-90"} transition-all `} />
             </button>
-            <ul className='my-2'>
+            <ul className={`my-2 ${!showSubmenu && "hidden"}`}>
               <li> 
-                  <Link to="/#" className='py-2 px-4 border-l text-gray-100 rounded-lg hover:bg-primary transition-colors border-green-300 ml-6 block
-                  relative before:w-3 before:h-3 before:absolute before:bg-green-400 before:rounded-full 
-                  before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-green-500 '>
+                  <Link to="/#" className='py-2 px-4 border-l text-gray-100 rounded-lg hover:bg-primary transition-colors 
+                  border-green-300 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-green-400 
+                  before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-green-500 '>
                     Perfil
                   </Link>
               </li>
@@ -63,6 +73,10 @@ function Sidebar() {
           </Link>
       </nav>
     </div>
+    <button onClick={() => setShowMenu(!showMenu)} className='xl:hidden fixed bottom-4 right-4 bg-primary text-white p-3 rounded-full z-50'>
+     {showMenu ? <RiCloseLine /> : <RiMenuLine /> } 
+    </button>
+    </>
   )
 }
 
