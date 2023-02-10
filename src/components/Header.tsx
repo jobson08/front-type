7e0399ab30cc03288487a67814f636b4c9816ae0
jobson <jammeloalves@gmail.com>
2023-02-10 11:22:46 -0300

    inicio estilo header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { RiArrowDownSLine, RiNotification3Line } from 'react-icons/ri';
+import avatar from '../data/avatar.jpg'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header className="h-[9vh] border-b p-5 border-gray-300 flex items-center justify-end "> 
+    <nav className='flex items-center gap-x-4'>
+     <button className='relative'>
+        <RiNotification3Line  className='w-5 h-5 '/>  
+        <span className='absolute -top-2 -right-1 bg-secondary py-0.5 px-[5px] box-content text-black rounded-full text-[8px] font-bold'>
+         2
+        </span>
+     </button>
+     <button className='flex items-center gap-x-2'>
+         <img src={avatar} alt="user" className='w-8 h-8 object-cover rounded-full'/>
+         <span>jobson</span>
+         <RiArrowDownSLine />
+     </button>
+     </nav>     
+ </header>
   )
 }
 
--- src/layouts/LayoutAdmin.tsx
+++ src/layouts/LayoutAdmin.tsx
@@ -1,4 +1,4 @@
-import Navbar from "components/Navbar"
+import Header from "components/Header"
 import Sidebar from "components/Sidebar"
 import { Outlet } from "react-router-dom"
 
@@ -8,7 +8,7 @@ function LayoutAdmin() {
     <div className="min-h-screen grid grid-col-1 xl:grid-cols-6 ">
       <Sidebar />
       <div className="xl:col-span-5">
-        <Navbar />
+        <Header />
         <div className="h-[90vh] overflow-y-scroll py-5 p-5 bg-body-bg">
           <Outlet />
         </div>
